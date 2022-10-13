823c380eca1a9fb8a62f16079bda91cfa1ac692a
Nicolas Delaval <delaval.n@gmail.com>
2022-10-13 21:48:43 +0200

    [Feature] Implementing persistence
    - Implemented Spring JPA
    
    - Implemented Flyway db
    
    - Basic persistence and flyway settings written within
    application.properties file
    
    - Run app : flyway scripting OK, table 'topfizz' and related
    rows are found in db

---
+++ target/classes/db/migration/V1__init.sql
@@ -0,0 +1,19 @@
+create table IF NOT EXISTS topfizz
+(
+	id bigserial constraint topfizz_pk primary key,
+	int1 int not null,
+	int2 int not null,
+	str1 text not null,
+	str2 text not null
+);
+DELETE from topfiZZ WHERE id is NOT null;
+ALTER SEQUENCE topfizz_id_seq RESTART WITH 1;
+insert into topfizz(int1,int2,str1,str2)
+values(5,7,'Le Bon','Coin'),
+       (2,3,'Fizz','Buzz'),
+       (10,21,'Cou','Cou'),
+       (6,5,'Bon','Jour'),
+       (5,7,'Le Bon','Coin'),
+       (9,4,'Le Bon','Coin'),
+       (2,1,'Ca','Nard'),
+	   (8,9,'Ca','Mion');
